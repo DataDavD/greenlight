fccf5b35a07a4adce6ec73a0374e6060235d1d73
David Dansby <39511285+DataDavD@users.noreply.github.com>
2021-10-12 00:49:16 -0700

    Make healthcheck endpoint return JSON response

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -6,7 +6,20 @@ import (
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	// Create a fixed-format JSON response from a string. Notice how we're using a row string
+	// literal (enclosed with backticks) so that we can include double-quote characters
+	// in the JSON without needing to escape them.
+	// We also use the %q verb to wrap the interpolated values in double-quotes
+	js := `{"status": "available", "environment": %q, "version": %q}`
+	js = fmt.Sprintf(js, app.config.env, version)
+
+	// Set the "Content-Type: application/json" header on the response. If we forget to do this,
+	// Go will default to sending a "Content-Type: text/plain; charset=utf-8" header instead.
+	w.Header().Set("Content-Type", "application/json")
+
+	// Write the JSON as the HTTP response body.
+	if _, err := w.Write([]byte(js)); err != nil {
+		app.logger.Println("error:", err)
+		return
+	}
 }
