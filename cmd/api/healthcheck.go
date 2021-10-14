cd1a43b99ca70c54ba64f1de6919d69f56bb3736
David Dansby <39511285+DataDavD@users.noreply.github.com>
2021-10-13 23:54:24 -0700

    Update healthcheck handler to use json.Marshal
    This commit updates the healthcheck handler to use json.Marshal() to
    generate a JSON response directly from a Go map instead of using a
    fixed-formed string like we had done previously.

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,20 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	// Create a fixed-format JSON response from a string. Notice how we're using a row string
-	// literal (enclosed with backticks) so that we can include double-quote characters
-	// in the JSON without needing to escape them.
-	// We also use the %q verb to wrap the interpolated values in double-quotes
-	js := `{"status": "available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+	// Create a map which holds the information that we want to send in a response.
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
 
-	// Set the "Content-Type: application/json" header on the response. If we forget to do this,
-	// Go will default to sending a "Content-Type: text/plain; charset=utf-8" header instead.
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request",
+			http.StatusInternalServerError)
+		return
+	}
+
+	// Append a newline to the JSON. This is just a small nicity to make it easier to view
+	// in terminal applications.
+	js = append(js, '\n')
+
+	// At this point we know that the encoding the data worked without any problems, so we can
+	// safely set any necessary HTTP headers for a successful response.
 	w.Header().Set("Content-Type", "application/json")
 
 	// Write the JSON as the HTTP response body.
