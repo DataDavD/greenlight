17754d2a6d85cc6f2c07430615ec9ea1c7824101
David Dansby <39511285+DataDavD@users.noreply.github.com>
2021-10-21 23:39:57 -0700

    Update showMovieHandler to use new Movie type

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/DataDavD/snippetbox/greenlight/internal/data"
 )
 
 // createMovieHandler handles "POST /v1/movies" endpoint. For now, we just return a plain-text
@@ -23,6 +26,25 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	// Otherwise, interpolate the movie ID in a placeholder response.
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+	// Create a new instance of the Movie struct, containing the ID we extracted from the URL and
+	// some dummy data. Also notice that we deliberately haven't set a value for the Year field.
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	// Encode the struct to JSON and send it as the HTTP response.
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encounterd a problem and could not process your request",
+			http.StatusInternalServerError)
+		return
+	}
+
+	return
 }
