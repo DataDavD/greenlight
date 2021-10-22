dc9953e1bdd6bb2a00014dda01acaef4376408a3
David Dansby <39511285+DataDavD@users.noreply.github.com>
2021-10-21 23:58:31 -0700

    Refactor struct tags in Movie type
    This commit adds the - struct tag directive to the CreatedAt Movie field
    since its not useful to end users. This commit also adds the "omitempty"
    struct tag directive to the Movie fields Year, Runtime, and Genres to
    ensure that empty values of these fields are hidden (i.e. Year of 0
    doesn't make sense if we don't know the Year of a Movie).

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -3,12 +3,12 @@ package data
 import "time"
 
 type Movie struct {
-	ID        int64     `json:"id"`         // Unique integer ID for the movie
-	CreatedAt time.Time `json:"created_at"` // Timestamp for when the movie is added to our database
+	ID        int64     `json:"id"` // Unique integer ID for the movie
+	CreatedAt time.Time `json:"-"`  // Use the - directive to never export in JSON output
 	Title     string    `json:"title"`
-	Year      int32     `json:"year"` // Movie release year
-	Runtime   int32     `json:"runtime"`
-	Genres    []string  `json:"genres"`
+	Year      int32     `json:"year,omitempty"` // Movie release year
+	Runtime   int32     `json:"runtime,omitempty"`
+	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"` // The version number starts at 1 and is incremented each
 	// time the movie information is updated.
 }
