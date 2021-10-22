402432675b6edef4a6d9368219a62d6cf1a1ecf8
David Dansby <39511285+DataDavD@users.noreply.github.com>
2021-10-21 23:44:31 -0700

    Add json tags to Movie data type
    This commit adds json tags to the Movie fields so that they are
    annotated according to JSON specification.

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -3,12 +3,12 @@ package data
 import "time"
 
 type Movie struct {
-	ID        int64     // Unique integer ID for the movie
-	CreatedAt time.Time // Timestamp for when the movie is added to our database
-	Title     string
-	Year      int32 // Movie release year
-	Runtime   int32
-	Genres    []string
-	Version   int32 // The version number starts at 1 and will be incremented each time the movie
-	// information is updated
+	ID        int64     `json:"id"`         // Unique integer ID for the movie
+	CreatedAt time.Time `json:"created_at"` // Timestamp for when the movie is added to our database
+	Title     string    `json:"title"`
+	Year      int32     `json:"year"` // Movie release year
+	Runtime   int32     `json:"runtime"`
+	Genres    []string  `json:"genres"`
+	Version   int32     `json:"version"` // The version number starts at 1 and is incremented each
+	// time the movie information is updated.
 }
