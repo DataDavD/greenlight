871b8620eb8e61dd216d7ff125907b145e076a18
David Dansby <39511285+DataDavD@users.noreply.github.com>
2021-10-21 23:39:12 -0700

    Refactor healthcheck handler
    Add explicit returns in healthcheck handler

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -17,5 +17,7 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 		app.logger.Println(err)
 		http.Error(w, "The server encountered a problem and could not process your request",
 			http.StatusInternalServerError)
+		return
 	}
+	return
 }
