1a6d66a848abef2504a4bd9df9077ce2ddb0a393
David Dansby <39511285+DataDavD@users.noreply.github.com>
2021-10-28 01:21:30 -0700

    Add custom error handlers to httprouter
    This commit adds our custom app.methodNotAllowedResponse and
    app.notFoundResponse methods to httprouter's corresponding custom
    handlers for dealing with routes that can't be matched or http methods
    that are not allowed for specific routes.

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,6 +9,14 @@ import (
 func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
+	// Convert the app.notFoundResponse helper to a http.Handler using the http.HandlerFunc()
+	// adapter, and then set it as the custom error handler for 404 Not Found responses.
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+
+	// Convert app.methodNotAllowedResponse helper to a http.Handler and set it as the custom
+	// error handler for 405 Method Not Allowed responses
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
