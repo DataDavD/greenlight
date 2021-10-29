bb3f489bf5bbe1878f41fb7905c280a9c111e3c4
David Dansby <39511285+DataDavD@users.noreply.github.com>
2021-10-29 00:04:53 -0700

    Refactor createMovieHandler
    This commit refactors createMovieHandler by implementing JSON Decoding
    using json.Decoder type.

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -11,11 +12,31 @@ import (
 // createMovieHandler handles "POST /v1/movies" endpoint. For now, we just return a plain-text
 // placeholder response.
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := fmt.Fprintln(w, "create a new movie")
+	// Declare an anonymous struct to hold the information that we expect to be in the HTTP
+	// request body (not that the field names and types in the struct are a subset of the Movie
+	// struct). This struct will be our *target decode destination*.
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	// Initialize a new json.Decoder instance which reads from teh request body, and then use the
+	// Decode() method to decode the body contents into the input struct. Importantly,
+	// notice that when we call Decode() we pass a *pointer* to the input struct as the target
+	// decode destination. If there is an error during decoding, we use our generic errorResponse()
+	// helper to send the client a 400 Bad Request response containing error message.
+	err := json.NewDecoder(r.Body).Decode(&input)
 	if err != nil {
-		app.logger.Println(err)
-		http.Error(w, "The server encounterd a problem and could not process your request",
-			http.StatusInternalServerError)
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	// Dump the contents of the input struct in an HTTP response.
+	_, err = fmt.Fprintf(w, "%+v\n", input)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
 		return
 	}
 }
@@ -24,7 +45,7 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 // placeholder response using the interpolated "id" parameter from the current URL
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	// When httprouter is parsing a request, any interpolated URL Parameters will be stored
-	// in the request context. We can use the ParamsFromContexnt() function to retrieve a slice
+	// in the request context. We can use the ParamsFromContext() function to retrieve a slice
 	// containing these paremter names and values.
 	id, err := app.readIDParam(r)
 	if err != nil || id < 1 {
