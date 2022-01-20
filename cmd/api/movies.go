d8b8dac11c6de0d15edf21c9d92b240f2b6ad40e
David Dansby <39511285+DataDavD@users.noreply.github.com>
2022-01-19 23:54:32 -0800

    Refactor validation checks to ValidateMovie helper

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -31,38 +31,20 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
+	// Copy the values from the input struct to a new Movie struct.
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
 	// Initialize a new Validator instance.
 	v := validator.New()
 
-	// Use the Check() method to execute our validation checks.
-	// This will add the provided key and error message to the errors map if the check does not
-	// evaluate to true. For example,
-	// in the first line here we "check that the title is not equal to the empty string".
-	// In the second, we "check that the length of the title is less than or equal to 500 bytes"
-	// and so on.
-	// Check input.Title
-	v.Check(input.Title != "", "title", "must be provided")
-	v.Check(len(input.Title) <= 500, "title", "must not be more than 500 bytes long")
-
-	// Check input.Year
-	v.Check(input.Year != 0, "year", "must be provided")
-	v.Check(input.Year >= 1888, "year", "must be greater than 1888")
-	v.Check(input.Year <= int32(time.Now().Year()), "year", "must not be in the future")
-
-	// Check input.Runtime
-	v.Check(input.Runtime != 0, "runtime", "must be provided")
-	v.Check(input.Runtime > 0, "runtime", "must be a positive integer")
-
-	// Check input.Genres
-	v.Check(input.Genres != nil, "genres", "must be provided")
-	v.Check(len(input.Genres) >= 1, "genres", "must contain at least 1 genre")
-	v.Check(len(input.Genres) <= 5, "genres", "must not contain more than 5 genres")
-	v.Check(validator.Unique(input.Genres), "genres", "must not contain duplicate values")
-
-	// Use the Valid() method to see if any of the checks failed. If they did,
-	// then use the failedValidationResponse() helper to send a response to the client,
-	// passing in the v.Errors map.
-	if !v.Valid() {
+	// Call the ValidateMovie() function and return a response containing the errors if any of
+	// the checks fail.
+	if data.ValidateMovie(v, movie); !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"time"
+
+	"github.com/DataDavD/snippetbox/greenlight/internal/validator"
+)
 
 // Movie type whose fields describe the movie.
 // Note that the Runtime type uses a custom Runtime type instead of int32. Furthermore, the omitempy
@@ -16,4 +20,27 @@ type Movie struct {
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"` // The version number starts at 1 and is incremented each
 	// time the movie information is updated.
+}
+
+// ValidateMovie runs validation checks on Movie type.
+func ValidateMovie(v *validator.Validator, movie *Movie) {
+	// Check movie.Title
+	v.Check(movie.Title != "", "title", "must be provided")
+	v.Check(len(movie.Title) <= 500, "title", "must not be more than 500 bytes long")
+
+	// Check input.Year
+	v.Check(movie.Year != 0, "year", "must be provided")
+	v.Check(movie.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(movie.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+
+	// Check input.Runtime
+	v.Check(movie.Runtime != 0, "runtime", "must be provided")
+	v.Check(movie.Runtime > 0, "runtime", "must be a positive integer")
+
+	// Check input.Genres
+	v.Check(movie.Genres != nil, "genres", "must be provided")
+	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
+	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
+
 }
