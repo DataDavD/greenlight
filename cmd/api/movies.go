7b1850d75561a643b4df8707694e0f4e1780914b
David Dansby <39511285+DataDavD@users.noreply.github.com>
2022-02-16 01:25:19 -0800

    Add MovieModel.Get() method to showMovieHandler

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/DataDavD/snippetbox/greenlight/internal/data"
 	"github.com/DataDavD/snippetbox/greenlight/internal/validator"
@@ -88,15 +88,19 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	// Create a new instance of the Movie struct, containing the ID we extracted from the URL and
-	// some dummy data. Also notice that we deliberately haven't set a value for the Year field.
-	movie := data.Movie{
-		ID:        id,
-		CreatedAt: time.Now(),
-		Title:     "Casablanca",
-		Runtime:   102,
-		Genres:    []string{"drama", "romance", "war"},
-		Version:   1,
+	// Call the Get() method to fetch the data for a specific movie.
+	// We also need to use the errors.Is()
+	// function to check if it returns a data.ErrRecordNotFound error,
+	// in which case we send a 404 Not Found response to the client.
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
 	}
 
 	// Create an envelope{"movie": movie} instance and pass it to writeJSON(), instead of passing
