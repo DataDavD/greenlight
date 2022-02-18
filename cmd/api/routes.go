1c5dbfb8bff80bd07a3a8d5a781a8ddbf36cd448
David Dansby <39511285+DataDavD@users.noreply.github.com>
2022-02-17 23:40:39 -0800

    Creat PUT route to update movies

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -20,6 +20,7 @@ func (app *application) routes() *httprouter.Router {
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
+	router.HandlerFunc(http.MethodPut, "/v1/movies/:id", app.updateMovieHandler)
 
 	return router
 }
