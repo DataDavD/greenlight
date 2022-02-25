458cb3dc9a349b8a214f4098daa48f672196bb4f
David Dansby <39511285+DataDavD@users.noreply.github.com>
2022-02-24 23:58:55 -0800

    Create new error helper function for edit conflict
    This commit creates a new error helper function for edit conflicts that
    returns a 409 Conflict response with a helpful plain-English error
    messaging describing that the database record could not be updated due
    to an edit conflict and to try again.

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -66,3 +66,8 @@ func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Reques
 func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
 	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
 }
+
+func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Request) {
+	message := "unable to update the record due to an edit conflict, please try again"
+	app.errorResponse(w, r, http.StatusConflict, message)
+}
