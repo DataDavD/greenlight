7e8aaf9a4dd948fe1338d1d6e1774b221558becc
David Dansby <39511285+DataDavD@users.noreply.github.com>
2022-02-27 14:46:28 -0800

    Implement SQL query timeout
    This commit implements SQL query timeouts with timeout contexts.

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"context"
 	"database/sql"
 	"errors"
 	"time"
@@ -39,15 +40,20 @@ func (m MovieModel) Insert(movie *Movie) error {
 		VALUES ($1, $2, $3, $4) 
 		RETURNING id, created_at, version`
 
+	// Create a context with a 3-second timeout.
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
 	// Create an args slice containing the values for the placeholder parameters from the movie
 	// struct. Declaring this slice immediately next to our SQL query helps to make it nice and
 	// clear *what values are being user where* in the query
 	args := []interface{}{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
 
-	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
+	return m.DB.QueryRowContext(ctx, query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
 // Get fetches a record from the movies table and returns the corresponding Movie struct.
+// It cancels the query call if the SQL query does not finish within 3 seconds.
 func (m MovieModel) Get(id int64) (*Movie, error) {
 	// The PostgreSQL bigserial type that we're using for the movie ID starts auto-incrementing
 	// at 1 by default, so we know that no movies will have ID values less tan that.
@@ -57,20 +63,23 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 		return nil, ErrRecordNotFound
 	}
 
-	// Update the query to return pg_sleep(10) as the first value. This is to mimic a
-	// long-running query.
 	query := `
-		SELECT PG_SLEEP(10), id, created_at, title, year, runtime, genres, version
+		SELECT id, created_at, title, year, runtime, genres, version
         FROM movies
  		WHERE id = $1`
 
-	// Declare a movie struct to hold the data returned by the query.
 	var movie Movie
 
-	// Importantly, update the Scan() parameters so taht the pg_sleep(10) return value
-	// is scanned into a []byte slice.
-	err := m.DB.QueryRow(query, id).Scan(
-		&[]byte{},
+	// Use the context.WithTimeout() function to create a context.Context which carries a 3-second
+	// timeout deadline. Note, that we're using the empty context.Background() as the
+	// 'parent' context.
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	// Defer cancel to make sure that we cancel the context before the Get() method returns
+	defer cancel()
+
+	// Use the QueryRowContext() method to execute the query, passing in the context with the
+	// deadline ctx as the first argument.
+	err := m.DB.QueryRowContext(ctx, query, id).Scan(
 		&movie.ID,
 		&movie.CreatedAt,
 		&movie.Title,
@@ -111,9 +120,13 @@ func (m MovieModel) Update(movie *Movie) error {
 		movie.Version, // Add the expected movie version.
 	}
 
+	// Create a context with a 3-second timeout.
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
 	// Execute the SQL query. If no matching row could be found, we know the movie version
 	// has changed (or the record has been deleted) and we return ErrEditConflict.
-	err := m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&movie.Version)
 	if err != nil {
 		switch {
 		case errors.Is(err, sql.ErrNoRows):
@@ -137,10 +150,14 @@ func (m MovieModel) Delete(id int64) error {
 		DELETE FROM movies
 		WHERE id = $1`
 
+	// Create a context with a 3-second timeout.
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
 	// Execute the SQL query using the Exec() method,
 	// passing in the id variable as the value for the placeholder parameter. The Exec(
 	// ) method returns a sql.Result object.
-	result, err := m.DB.Exec(query, id)
+	result, err := m.DB.ExecContext(ctx, query, id)
 	if err != nil {
 		return err
 	}
