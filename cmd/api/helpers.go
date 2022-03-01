8967808789d3d49635bfa466132c91f44b8b6ef6
David Dansby <39511285+DataDavD@users.noreply.github.com>
2022-02-28 22:27:58 -0800

    Create helper functions for parsing URL queries.

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -6,9 +6,11 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"strconv"
 	"strings"
 
+	"github.com/DataDavD/snippetbox/greenlight/internal/validator"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -158,11 +160,59 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst int
 	return nil
 }
 
-// func deferFatalErrorFunc(app *application, err error) {
-// 	func() {
-// 		err := app.config.db.Close()
-// 		if err != nil {
-// 			app.infoLog.Fatal(err)
-// 		}
-// 	}()
-// }
+// readString is a helper method on application type that returns a string value from the URL query
+// string, or the provided default value if no matching key is found.
+func (app *application) readStrings(qs url.Values, key string, defaultValue string) string {
+	// Extract the value for a given key from the URL query string.
+	// If no key exists this will return an empty string "".
+	s := qs.Get(key)
+
+	// If no key exists (or the value is empty) then return the default value
+	if s == "" {
+		return defaultValue
+	}
+
+	// Otherwise, return the string
+	return s
+}
+
+// readCSV is a helper method on application type that reads a string value from the URL query
+// string and then splits it into a slice on the comma character. If no matching key is found
+// then it returns the provided default value.
+func (app *application) readCSV(qs url.Values, key string, defaultvalue []string) []string {
+	// Extract the value from the URL query string
+	csv := qs.Get(key)
+
+	// if no key exists (or the value is empty) then return the default value
+	if csv == "" {
+		return defaultvalue
+	}
+
+	// Otherwise, parse the value into a []string slice and return it.
+	return strings.Split(csv, ",")
+}
+
+// readInt is a helper method on application type that reads a string value from the URL query
+// string and converts it to an integer before returning. If no matching key is found then it
+// returns the provided default value. If the value couldn't be converted to an integer, then we
+// record an error message in the provided Validator instance, and return the default value.
+func (app *application) readInt(qs url.Values, key string, defaultValue int, v *validator.Validator) int {
+	// Extract the value from the URL query string.
+	s := qs.Get(key)
+
+	// If no key exists (or the value is empty) then return the default value.
+	if s == "" {
+		return defaultValue
+	}
+
+	// Try to convert the string value to an int. If this fails, add an error message to the
+	// validator instance and return the default value.
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		v.AddError(key, "must be an integer value")
+		return defaultValue
+	}
+
+	// Otherwise, return the converted integer value.
+	return i
+}
