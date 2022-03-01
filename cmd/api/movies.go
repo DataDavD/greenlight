6bfd01579fbf7105a138f3f6ceba8b22a842678f
David Dansby <39511285+DataDavD@users.noreply.github.com>
2022-02-28 22:49:03 -0800

    Create listMoviesHandler to list out movies

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -244,3 +244,47 @@ func (app *application) deleteMovieHandler(w http.ResponseWriter, r *http.Reques
 		app.serverErrorResponse(w, r, err)
 	}
 }
+
+func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request) {
+	// To keep things consistent with our other handlers, we'll define an input struct to hold
+	// the expected values from the request query string.
+	var input struct {
+		Title    string
+		Genres   []string
+		Page     int
+		Pagesize int
+		Sort     string
+	}
+
+	// Initialize a new Validator instance.
+	v := validator.New()
+
+	// call r.URL.Query() to get the url.Values map containing the query string data.
+	qs := r.URL.Query()
+
+	// Use our helpers to extract the title and genres query string values, falling back to defaults
+	// of an empty string and an empty slice, respectively, if they are not provided by the client.
+	input.Title = app.readStrings(qs, "title", "")
+	input.Genres = app.readCSV(qs, "genres", []string{})
+
+	// Ge the page and page_size query string value as integers. Notice that we set the default
+	// page value to 1 and default page_size to 20, and that we pass the validator instance
+	// as the final argument.
+	input.Page = app.readInt(qs, "page", 1, v)
+	input.Pagesize = app.readInt(qs, "page_size", 20, v)
+
+	// Extract the sort query string value, falling back to "id" if it is not provided
+	// by the client (which will imply an ascending sort on movie ID).
+	input.Sort = app.readStrings(qs, "sort", "id")
+
+	// Check the Validator instance for any errors and use the failedValidationResponse()
+	// helper to send the client a response if necessary.
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	// Dump the contents of the input struct in a HTTP response.
+	fmt.Fprintf(w, "%+v\n", input)
+
+}
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -18,6 +18,8 @@ func (app *application) routes() *httprouter.Router {
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+
+	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
