287e6f115a281b50f80bbcac0f2f13757d425f91
David Dansby <39511285+DataDavD@users.noreply.github.com>
2022-03-01 22:51:57 -0800

    Refactor

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -179,22 +179,22 @@ func (m MovieModel) Delete(id int64) error {
 	return nil
 }
 
-// ValidateMovie runs validation checks on Movie type.
+// ValidateMovie runs validation checks on the Movie type.
 func ValidateMovie(v *validator.Validator, movie *Movie) {
 	// Check movie.Title
 	v.Check(movie.Title != "", "title", "must be provided")
 	v.Check(len(movie.Title) <= 500, "title", "must not be more than 500 bytes long")
 
-	// Check input.Year
+	// Check movie.Year
 	v.Check(movie.Year != 0, "year", "must be provided")
 	v.Check(movie.Year >= 1888, "year", "must be greater than 1888")
 	v.Check(movie.Year <= int32(time.Now().Year()), "year", "must not be in the future")
 
-	// Check input.Runtime
+	// Check movie.Runtime
 	v.Check(movie.Runtime != 0, "runtime", "must be provided")
 	v.Check(movie.Runtime > 0, "runtime", "must be a positive integer")
 
-	// Check input.Genres
+	// Check movie.Genres
 	v.Check(movie.Genres != nil, "genres", "must be provided")
 	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
 	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
