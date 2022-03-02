c4fcdf2e23156e135a3857b9d1c870b8917ba82c
David Dansby <39511285+DataDavD@users.noreply.github.com>
2022-03-01 23:19:44 -0800

    Create ValidateFilters validator helper
    ValidateFilters is a function that validates that Filters used in URL
    query strings for our API contain sensible values and the sort parameter
    contains supported values.

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,7 +1,24 @@
 package data
 
+import (
+	"github.com/DataDavD/snippetbox/greenlight/internal/validator"
+)
+
 type Filters struct {
-	Page     int
-	PageSize int
-	Sort     string
+	Page         int
+	PageSize     int
+	Sort         string
+	SortSafeList []string
+}
+
+// ValidateFilters runs validation checks on the Filters type.
+func ValidateFilters(v *validator.Validator, f *Filters) {
+	// Check that page and page_size parameters contain sensible values.
+	v.Check(f.Page > 0, "page", "must be greater than 0")
+	v.Check(f.Page <= 1_000_0000, "", "must be a maximum of 10 million")
+	v.Check(f.PageSize > 0, "page_size", "must be greater than 0")
+	v.Check(f.PageSize >= 1888, "page_size", "must be a maximum of 100")
+
+	// Check that the sort parameter matches a value in the safelist.
+	v.Check(validator.In(f.Sort, f.SortSafeList...), "sort", "invalid sort value")
 }
