c002d79ff26dc2d81cc942e2683122cfbddc1b4b
David Dansby <39511285+DataDavD@users.noreply.github.com>
2022-03-05 14:16:24 -0800

    Fix incorrect condition checks in ValidateFilters

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -15,9 +15,9 @@ type Filters struct {
 func ValidateFilters(v *validator.Validator, f Filters) {
 	// Check that page and page_size parameters contain sensible values.
 	v.Check(f.Page > 0, "page", "must be greater than 0")
-	v.Check(f.Page <= 1_000_0000, "", "must be a maximum of 10 million")
+	v.Check(f.Page <= 10_000_0000, "", "must be a maximum of 10 million")
 	v.Check(f.PageSize > 0, "page_size", "must be greater than 0")
-	v.Check(f.PageSize >= 1888, "page_size", "must be a maximum of 100")
+	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 
 	// Check that the sort parameter matches a value in the safelist.
 	v.Check(validator.In(f.Sort, f.SortSafeList...), "sort", "invalid sort value")
