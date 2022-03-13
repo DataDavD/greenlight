052cb18ee9bf49108d8462a6a4d6f3b1f6e1712d
David Dansby <39511285+DataDavD@users.noreply.github.com>
2022-03-12 23:49:00 -0800

    Implement full-text search on MovieModel.GetAll

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -194,7 +194,7 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 	query := `
         SELECT id, created_at, title, year, runtime, genres, version
   		FROM movies
-        WHERE (LOWER(title) = LOWER($1) OR $1 = '')
+        WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
         AND (genres @> $2 OR $2 = '{}')
 		ORDER BY id
 		`
