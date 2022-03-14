a08be2945c2cb6054cb84ea4c90b779de8780abf
David Dansby <39511285+DataDavD@users.noreply.github.com>
2022-03-13 17:27:44 -0700

    Create sortColumn & sortDirection methods
    This commit creates two methods on Filters, sortColumn and
    sortDirection.
    
    sortColumn checks that the Sort field provided by the
    client matches one of the entries in the SortSafeList. If it does, then
    it extracts the column name by stripping the leading hyphen character
    (if it exists).
    
    sortDirection returns the sort direction ("ASC" or "DESC") depending on
    the prefix character of the Sort field.

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,8 @@
 package data
 
 import (
+	"strings"
+
 	"github.com/DataDavD/snippetbox/greenlight/internal/validator"
 )
 
@@ -21,4 +23,26 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 
 	// Check that the sort parameter matches a value in the safelist.
 	v.Check(validator.In(f.Sort, f.SortSafeList...), "sort", "invalid sort value")
+}
+
+// sortColumn checks that the client-provided Sort field matches one of the entries in our
+// SortSafeList and if it does, it extracts the column name from the Sort field by stripping the
+// leading hyphen character (if one exists).
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafeList {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+
+	panic("unsafe sort parameter:" + f.Sort)
+}
+
+// sortDirection returns the sort direction ("ASC" or "DESC") depending on the prefix character
+// of the Sort field.
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
 }
