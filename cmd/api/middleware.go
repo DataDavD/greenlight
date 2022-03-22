65cb5f624ecc20c2d4253e70cafdd59e5c1989ea
David Dansby <39511285+DataDavD@users.noreply.github.com>
2022-03-21 23:59:15 -0700

    Create middleware to recover from panic nicely

---
+++ cmd/api/middleware.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// recoverPanic is middleware that recovers from a panic by responding with a 500 Internal Server
+// Error before closing the connection. It will also log the error using our custom Logger at
+// the ERROR level.
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Create a deferred function (which will always be run in the event of a panic as
+		// Go unwinds the stack).
+		defer func() {
+			// Use the builtin recover function to check if there has been a panic or not.
+			if err := recover(); err != nil {
+				// If there was a panic, set a "Connection: close" header on the response. This
+				// acts a a trigger to make Go's HTTP server automatically close the current
+				// connection after a response has been sent.
+				w.Header().Set("Connection:", "close")
+				// The value returned by recover() has the type interface{}, so we use
+				// fmt.Errorf() to normalize it into an error and call our
+				// serverErrorResponse() helper. In turn, this will log the error using our
+				// custom Logger type at the ERROR level and send the client a
+				// 500 Internal Server Error response.
+				app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
