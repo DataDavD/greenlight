585f2da460ab4c96640ed5ac51f47603ceac3360
David Dansby <39511285+DataDavD@users.noreply.github.com>
2022-03-21 23:27:50 -0700

    Add omitempty on jsonLogger Properties and Trace

--- internal/jsonlog/jsonlog.go
+++ internal/jsonlog/jsonlog.go
@@ -82,8 +82,8 @@ func (l *Logger) print(level Level, message string, properties map[string]string
 		Level      string            `json:"level"`
 		Time       string            `json:"time"`
 		Message    string            `json:"message"`
-		Properties map[string]string `json:"properties"`
-		Trace      string            `json:"trace"`
+		Properties map[string]string `json:"properties,omitempty"`
+		Trace      string            `json:"trace,omitempty"`
 	}{
 		Level:      level.String(),
 		Time:       time.Now().UTC().Format(time.RFC3339),
