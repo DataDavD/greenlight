ef67194d7adc98f0bbec80a1cbe40ee18222390d
David Dansby <39511285+DataDavD@users.noreply.github.com>
2022-03-27 17:08:57 -0700

    Update server start to intercept SIGINT & SIGTERM
    Update the server starting function to intercept SIGINT and SIGTERM
    signals, log that we caught such signals and exit the app with status
    code 0 (success). We do not catch SIGQUIT, because we want to have
    the ability to quickly execute a "non-graceful" shutdown via a keyboard.
    We may refactor this in the future.

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -18,6 +21,31 @@ func (app *application) serve() error {
 		WriteTimeout: 30 * time.Second,
 	}
 
+	// Start a background goroutine.
+	go func() {
+		// Create a quit channel which carries os.Signal values. Use buffered
+		quit := make(chan os.Signal, 1)
+
+		// Use signal.Notify() to listen for incoming SIGINT and SIGTERM signals and relay
+		// them to the quit channel. Any other signal will not be caught by signal.Notify()
+		// and will retain their default behavior.
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		// Read the signal from the quit channel. This code will block until a signal is
+		// received.
+		s := <-quit
+
+		// Log a message to say that the signal has been caught. Notice that we also call the
+		// String() method on the signal to get the signal name and include it in the log
+		// entry properties.
+		app.logger.PrintInfo("caught signal", map[string]string{
+			"signal": s.String(),
+		})
+
+		// Exit the application with a 0 (success) status code.
+		os.Exit(0)
+	}()
+
 	// Log a "starting server" message.
 	app.logger.PrintInfo("starting server", map[string]string{
 		"addr": srv.Addr,
@@ -26,5 +54,4 @@ func (app *application) serve() error {
 
 	// Start the server as normal, returning any error.
 	return srv.ListenAndServe()
-
 }
