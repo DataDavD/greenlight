850d06527bff3f0a9921b7c193c38c87f982a9fe
David Dansby <39511285+DataDavD@users.noreply.github.com>
2022-04-10 13:53:12 -0700

    Create UserModel
    Create the UserModel to interact with the users table in our database.
    We also create a few methods for interacting with the users table.
    Specifically, they are Insert, Update, and GetByEmail.

--- internal/data/models.go
+++ internal/data/models.go
@@ -18,6 +18,7 @@ var (
 // Models struct is a single convenient container to hold and represent all our database models.
 type Models struct {
 	Movies MovieModel
+	Users  UserModel
 }
 
 func NewModels(db *sql.DB) Models {
@@ -25,6 +26,11 @@ func NewModels(db *sql.DB) Models {
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 	return Models{
 		Movies: MovieModel{
+			DB:       db,
+			InfoLog:  infoLog,
+			ErrorLog: errorLog,
+		},
+		Users: UserModel{
 			DB:       db,
 			InfoLog:  infoLog,
 			ErrorLog: errorLog,
