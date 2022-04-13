58d6b32112f30b280f5d29a93fb54357168f8ec6
David Dansby <39511285+DataDavD@users.noreply.github.com>
2022-04-12 23:36:16 -0700

    Create/Add registerUserHandler

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -20,11 +20,15 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 
+	// Movies handlers
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
+
+	// Users handlers
+	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 
 	// Wrap the router with the panic recovery middleware and rate limit middleware.
 	return app.recoverPanic(app.rateLimit(router))
---
+++ cmd/api/users.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/DataDavD/snippetbox/greenlight/internal/data"
+	"github.com/DataDavD/snippetbox/greenlight/internal/validator"
+)
+
+func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
+	// Create an anonymous struct to hold the expected data from the request body.
+	var input struct {
+		Name     string `json:"name"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	// Parse the request body into the anonymous struct
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	// Copy the data from the request body into a new User struct. Notice also that
+	// we set the Activated field to false, which isn't strictly necessary because
+	// the Activated field will have the zero-value of false by default. But setting
+	// this explicitly helps to make our intentions clear to anyone reading the code.
+	user := &data.User{
+		Name:      input.Name,
+		Email:     input.Email,
+		Activated: false,
+	}
+
+	// Use the Password.Set() method to generate and store the hashed and plaintext
+	// passwords.
+	err = user.Password.Set(input.Password)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+
+	// Validate the user struct and return the error messages to the client if
+	// any of the checks fail.
+	if data.ValidateUser(v, user); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	// Insert the user data into the database.
+	err = app.models.Users.Insert(user)
+	if err != nil {
+		switch {
+		// If we get an ErrDuplicateEmail error, use the v.AddError() method to manually add
+		// a message to the validator instance, and then call our failedValidationResponse
+		// helper().
+		case errors.Is(err, data.ErrDuplicateEmail):
+			v.AddError("email", "a user with this email address already exists")
+			app.failedValidationResponse(w, r, v.Errors)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	// Write a JSON response containing the user data along with a 201 Created status code.
+	err = app.writeJSON(w, 201, envelope{"user": user}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
