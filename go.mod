4519f8788cf6708ebdd2e9856cfb76fc500788d4
David Dansby <39511285+DataDavD@users.noreply.github.com>
2022-04-16 13:30:15 -0700

    Create Mailer type & a Send method to send emails

--- go.mod
+++ go.mod
@@ -3,8 +3,14 @@ module github.com/DataDavD/snippetbox/greenlight
 go 1.18
 
 require (
-	github.com/julienschmidt/httprouter v1.3.0 // indirect
-	github.com/lib/pq v1.10.4 // indirect
-	golang.org/x/crypto v0.0.0-20220408190544-5352b0902921 // indirect
-	golang.org/x/time v0.0.0-20220224211638-0e9765cccd65 // indirect
+	github.com/go-mail/mail/v2 v2.3.0
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/lib/pq v1.10.4
+	golang.org/x/crypto v0.0.0-20220408190544-5352b0902921
+	golang.org/x/time v0.0.0-20220224211638-0e9765cccd65
+)
+
+require (
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/mail.v2 v2.3.1 // indirect
 )
---
+++ internal/mailer/mailer.go
@@ -0,0 +1,92 @@
+package mailer
+
+import (
+	"bytes"
+	"embed"
+	"html/template"
+	"time"
+
+	"github.com/go-mail/mail/v2"
+)
+
+// Below we declare a new variable with the type embed.FS (embedded file system) to hold
+// our email templates. This has a comment directive in the format `//go:embed <path>`
+// IMMEDIATELY ABOVE it, which indicates to Go that we want to store the contents of the
+// ./templates directory in the templateFS embedded file system variable.
+//go:embed "templates"
+var templateFS embed.FS
+
+// Mailer contains a mail.Dialer instance (used to connect to an SMTP server)
+// and the sender information for our emails (the name and address we want the email to be from,
+// such as "Alice Smith <alice@example.com>").
+type Mailer struct {
+	dialer *mail.Dialer
+	sender string
+}
+
+// New initializes a new mail.Dialer instance with the given SMTP server settings and a 5-second
+// timeout whenever we send an email. It returns a Mailer instance containing the dialer and sender
+// information.
+func New(host string, port int, username, password, sender string) Mailer {
+	dialer := mail.NewDialer(host, port, username, password)
+	dialer.Timeout = 5 * time.Second
+
+	return Mailer{
+		dialer: dialer,
+		sender: sender,
+	}
+}
+
+func (m Mailer) Send(recipient, templateFile string, data interface{}) error {
+	// Use the ParseFS() method to parse the required template file from the embedded
+	// file system.
+	tmpl, err := template.New("email").ParseFS(templateFS, "templates/"+templateFile)
+	if err != nil {
+		return err
+	}
+
+	// Execute the named template "subject", passing in the dynamic data and storing the
+	// result in a bytes.Buffer variable.
+	subject := new(bytes.Buffer)
+	err = tmpl.ExecuteTemplate(subject, "subject", data)
+	if err != nil {
+		return err
+	}
+
+	// Execute the named template "plainBody" and store in the result in a plainBody
+	// variable.
+	plainBody := new(bytes.Buffer)
+	err = tmpl.ExecuteTemplate(plainBody, "plainBody", data)
+	if err != nil {
+		return err
+	}
+
+	// Execute the named template "htmlBody" similar to above.
+	htmlBody := new(bytes.Buffer)
+	err = tmpl.ExecuteTemplate(htmlBody, "htmlBody", data)
+	if err != nil {
+		return err
+	}
+
+	// Use the mail.NewMessage() function to initialize a new mail.Message instance.
+	// Then use the SetHeader() method to set the mail recipient, sender, and subject headers,
+	// the SetBody() method to set the plain-text body, and the AddAlternative() method to set
+	// the HTML body. It's important to note that AddAlernative() should always be called *after*
+	// SetBody().
+	msg := mail.NewMessage()
+	msg.SetHeader("To", recipient)
+	msg.SetHeader("From", m.sender)
+	msg.SetHeader("Subject", subject.String())
+	msg.SetBody("text/plain", plainBody.String())
+	msg.AddAlternative("text/html", htmlBody.String())
+
+	// Call the DialAndSend() method on the dialer, passing in the message to send.
+	// This opens a connection to the SMTP server, sends the message, then closes the connection.
+	// If there is a timeout, it will return a "dial tcp: i/o timeout" error.
+	err = m.dialer.DialAndSend(msg)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
