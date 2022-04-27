2177bebeab6e470ed1f245eb3eeb3ca1f84226d8
David Dansby <39511285+DataDavD@users.noreply.github.com>
2022-04-27 00:15:11 -0700

    create/add createAuthenticationTokenHandler

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -31,6 +31,9 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 
+	// Tokens handlers
+	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
+
 	// Wrap the router with the panic recovery middleware and rate limit middleware.
 	return app.recoverPanic(app.rateLimit(router))
 }
---
+++ cmd/api/tokens.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"time"
+
+	"github.com/DataDavD/snippetbox/greenlight/internal/data"
+	"github.com/DataDavD/snippetbox/greenlight/internal/validator"
+)
+
+func (app *application) createAuthenticationTokenHandler(w http.ResponseWriter, r *http.Request) {
+	// Parse the email and password from the request body.
+
+	var input struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	// Validate the email and password provided by the client.
+	v := validator.New()
+	data.ValidateEmail(v, input.Email)
+	data.ValidatePasswordPlaintext(v, input.Password)
+
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	// Lookup the user record based on the email address. If no matching user was found, then we
+	// call the app.invalidCredentialsResponse() helper to send a 501 Unauthorized response to
+	// the client.
+	user, err := app.models.Users.GetByEmail(input.Email)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.invalidCredentialsResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	// Check if the provided password matches the actual password for the user.
+	match, err := user.Password.Matches(input.Password)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	// If the passwords don't match, then call the app.invalidCredentialsResponse() helper
+	// and return
+	if !match {
+		app.invalidCredentialsResponse(w, r)
+		return
+	}
+
+	// Otherwise, if the password is correct, we generate a new token with a 24-hour expiry time
+	// and the scope 'authentication'.
+	token, err := app.models.Tokens.New(user.ID, 24*time.Hour, data.ScopeAuthentication)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	// Encode the token to JSON and send it in the response along with a 201 Created status code.
+	err = app.writeJSON(w, http.StatusCreated, envelope{"authentication_token": token}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
