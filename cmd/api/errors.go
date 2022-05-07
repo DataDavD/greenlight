208b207813b68b9f74fbc133f9a46a3eb729577a
David Dansby <39511285+DataDavD@users.noreply.github.com>
2022-05-06 23:10:03 -0700

    Create new notPermittedResponse error helper

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -112,3 +112,8 @@ func (app *application) inactiveAccountResponse(w http.ResponseWriter, r *http.R
 	message := "your user account must be activated to access this resource"
 	app.errorResponse(w, r, http.StatusForbidden, message)
 }
+
+func (app *application) notPermittedResponse(w http.ResponseWriter, r *http.Request) {
+	message := "your user account doesn't have the necessary permissions to access this resource"
+	app.errorResponse(w, r, http.StatusForbidden, message)
+}
