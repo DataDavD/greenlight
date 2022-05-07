67fe54b801de31c310f4841c3b6a81280c93a62e
David Dansby <39511285+DataDavD@users.noreply.github.com>
2022-05-06 23:59:22 -0700

    Update registerUserHandler
    registerUserHandler now adds 'movies:read' permissions to every
    registered user.

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -67,6 +67,13 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	// Add the "movies:read" permission for the new user.
+	err = app.models.Permissions.AddForUser(user.ID, "movies:read")
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
 	// After the user record has been created in the database, generate a new activation
 	// token for the user.
 	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
